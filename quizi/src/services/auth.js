c545c55546c13e95714067a42b3bd364301e3c8f
feveron <veronika.fedyk06@gmail.com>
2025-04-07 02:11:46 +0300

    created readme.txt with info

--- quizi/src/services/auth.js
+++ quizi/src/services/auth.js
@@ -74,67 +74,6 @@ class Auth_Service {
 
   }
 
-  
-  //     IF @user_service.get("/user_info/{user_id}") IS ACTIVE ON BACKEND  (LOOK IN VIEWS.PY OR CONTACT DANIEL_KAISER)
-
-  // async get_user_info() {
-  //   try {
-  //     const user_id = get_user_id();
-  //     const response = await axios.get(`${base_url}/user_info/${user_id}`);
-
-  //     if (response.status === 200) {
-
-  //       console.log("User info retrieved:", response.data);
-  //       return { success: true, data: response.data };
-  //     }
-  //   } catch (error) {
-  //     if (error.response) {
-  //       if (error.response.status === 404) {
-  //         alert('User not found.');
-  //         return { success: false, message: 'User not found.' };
-  //       }
-  //       alert('An unexpected error occurred.');
-  //       return { success: false, message: 'An error occurred.' };
-  //     } else {
-  //       console.error("Error fetching user info:", error);
-  //       return { success: false, message: 'Network error.' };
-  //     }
-  //   }
-  // }
-
-
-  //      IF @user_service.post("/get_user_info") IS ACTIVE ON BACKEND  (LOOK IN VIEWS.PY OR CONTACT DANIEL_KAISER)
-
-  // async get_user_info() {
-  //     try {
-  //     const user_id = get_user_id();
-  //     //console.log('User ID:', user_id);
-
-  //     const response = await axios.post(base_url + "/get_user_info", { user_SID: user_id, });
-
-  //     if (response.status === 200) {
-
-  //       console.log("User info retrieved:", response.data);
-  //       return { success: true, data: response.data };
-  //     }
-  //   } catch (error) {
-  //     if (error.response) {
-  //       if (error.response.status === 404) {
-
-  //         alert('User not found.');
-  //         return { success: false, message: 'User not found.' };
-  //       }
-  //       alert('An unexpected error occurred.');
-  //       return { success: false, message: 'An error occurred.' };
-
-  //     } else {
-  //       console.error("Error fetching user info:", error);
-  //       return { success: false, message: 'Network error.' };
-  //     }
-  //   }
-  // }
-
-
 }
 
 const authService = new Auth_Service();
